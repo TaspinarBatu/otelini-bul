81a8aaca9589afb30b127647e53cc086fb23d92d
Batuhan Taşpınar <batuhant_52@icloud.com>
2024-05-09 02:10:46 +0300

    Uygulamanın güncel hali

---
+++ app/src/main/java/com/ankagroup/otelinibul/GirisYapSayfasi.kt
@@ -0,0 +1,11 @@
+package com.ankagroup.otelinibul
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GirisYapSayfasi : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.giris_yap)
+    }
+}
